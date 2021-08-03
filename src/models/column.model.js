880b5083a998b37e63321b0be66452cb25a937ce
Nguyen Ngoc Bang <bang270197@gmail.com>
2021-08-03 10:04:25 +0700

    update column and create card

--- src/models/column.model.js
+++ src/models/column.model.js
@@ -1,9 +1,11 @@
 import Joi from "joi";
+// import { ObjectId } from "mongodb";
+const { ObjectId } = require("mongodb");
 import { getDB } from "*/config/mongodb.js";
-const columnCollectionName = "Columns";
+const columnCollectionName = "columns";
 const columnCollectionSchema = Joi.object({
     boardId: Joi.string().required(),
-    title: Joi.string().required().min(3).max(20),
+    title: Joi.string().required().min(3).max(20).trim().trim(),
     cardOrder: Joi.array().items(Joi.string()).default([]),
     createAt: Joi.date().timestamp().default(Date.now()),
     updateAt: Joi.date().timestamp().default(null),
@@ -24,8 +26,26 @@ const createNew = async (data) => {
             .insertOne(value);
         return result;
     } catch (error) {
-        console.log(error);
+        throw new Error(error);
     }
 };
 
-export const ColumnModel = { createNew };
+const update = async (id, data) => {
+    try {
+        // const value = await validateSchema(data);
+        const result = await getDB()
+            .collection(columnCollectionName)
+            .findOneAndUpdate(
+                {
+                    _id: ObjectId(id),
+                },
+                { $set: data },
+                { returnOriginal: false }
+            );
+        return result.value;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const ColumnModel = { createNew, update };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,15 @@ import express from "express";
 var router = express.Router();
 import { HttpStatusCode } from "*/utilities/constants";
 import { boardsRoutes } from "./board.routes";
-
+import { columnRoutes } from "./column.routes";
+import { cardRoutes } from "./card.routes";
 router.get("/status", (req, res) => {
     res.status(HttpStatusCode.OK).json({ status: "OK" });
 });
 
-router.use("/board", boardsRoutes);
+router.use("/boards", boardsRoutes);
 
+router.use("/columns", columnRoutes);
+
+router.use("/cards", cardRoutes);
 export const apiV1 = router;
